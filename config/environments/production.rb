21acdf861e3be31dc6eadc5626d3c098a0270626
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2016-11-27 17:35:42 -0800

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'afternoon-atoll-97767.herokuapp.com/', :protocol => 'https'}
+  #config.action_mailer.default_url_options = { :host => 'afternoon-atoll-97767.herokuapp.com/', :protocol => 'https'}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
